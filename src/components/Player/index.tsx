09f83b80480bca48949646b9118a8e76b6c778e1
mat20 <matheusouza12@gmail.com>
2021-06-10 11:11:21 -0300

    PodCastr

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,145 @@
+import { useEffect, useRef } from 'react';
+import { usePlayer } from '../../contexts/PlayerContext';
+import { DurationToTimeString } from '../../utils/DurationToTimeString';
+
+import Image from 'next/image';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
+import styles from './styles.module.scss';
+
+
+export function Player() {
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const {
+    episodeList,
+    currentEpisodeIndex,
+    isPlaying,
+    isLooping,
+    isShuffling,
+    toggleShuffle,
+    togglePlay,
+    toggleLooping,
+    setPlayingState,
+    playNext,
+    playPrevious,
+    hasNext,
+    hasPrevious
+  } = usePlayer();
+
+  useEffect(() => {
+    if (!audioRef.current) {
+      return;
+    }
+
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying])
+
+  const episode = episodeList[currentEpisodeIndex]
+
+  return (
+    <div className={styles.playerContainer}>
+      <header>
+        <img src="/playing.svg" alt="Tocando agora" />
+        <strong>Tocando agora</strong>
+      </header>
+
+      { episode ? (
+        <div className={styles.currentEpisode}>
+          <Image
+            width={592}
+            height={592}
+            src={episode.thumbnail}
+            objectFit="cover"
+          />
+          <strong>{episode.title}</strong>
+          <span>{episode.members}</span>
+        </div>
+      ) : (
+        <div className={styles.emptyPlayer}>
+          <strong>Selecione um podcast para ouvir</strong>
+        </div>
+      )}
+
+      <footer className={!episode ? styles.empty : ''}>
+        <div className={styles.progress}>
+          <span>{}</span>
+          <div className={styles.slider}>
+            {episode ? (
+              <Slider
+                trackStyle={{ background: '#04d361' }}
+                railStyle={{ background: '#9f75ff' }}
+                handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
+              />
+            ) : (
+              <div className={styles.emptySlider} />
+            )}
+          </div>
+          <span>{DurationToTimeString(episode?.durantion ?? 0)}</span>
+        </div>
+
+        {episode && (
+          <audio
+            src={episode.url}
+            ref={audioRef}
+            loop={isLooping}
+            autoPlay
+            onPlay={() => setPlayingState(true)}
+            onPause={() => setPlayingState(false)}
+          />
+        )}
+
+        <div className={styles.buttons}>
+          <button
+            type="button"
+            disabled={!episode || episodeList.length === 1}
+            onClick={toggleShuffle}
+            className={isShuffling ? styles.isActive : ''}
+          >
+            <img src="/shuffle.svg" alt="Embaralhar" />
+          </button>
+          <button
+            type="button"
+            onClick={playPrevious}
+            disabled={!episode || !hasPrevious}
+          >
+            <img src="/play-previous.svg" alt="Tocar anterior" />
+          </button>
+          <button
+            type="button"
+            className={styles.playButton}
+            disabled={!episode}
+            onClick={togglePlay}
+          >
+            {isPlaying
+              ? <img src="/pause.svg" alt="Pause" />
+              : <img src="/play.svg" alt="Tocar" />
+            }
+          </button>
+          <button
+            type="button"
+            onClick={playNext}
+            disabled={!episode || !hasNext}
+          >
+            <img src="/play-next.svg" alt="Tocar próxima" />
+          </button>
+          <button
+            type="button"
+            disabled={!episode}
+            onClick={toggleLooping}
+            className={isLooping ? styles.isActive : ''}
+          >
+            <img src="/repeat.svg" alt="Repetir" />
+          </button>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+
